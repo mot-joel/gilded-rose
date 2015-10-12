5b7ff445da9c91be311749bb9a88d4937fcd4046
Jordan Ryan Reuter <me@jreut.com>
2015-10-12 11:17:46 -0400

    Remove `item` variable from #tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,47 +12,46 @@ class GildedRose
   end
 
   def tick
-    item = self
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
+    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAFKAL80ETC concert'
+      if @quality > 0
+        if @name != 'Sulfuras, Hand of Ragnaros'
+          @quality -= 1
         end
       end
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
+      if @quality < 50
+        @quality += 1
+        if @name == 'Backstage passes to a TAFKAL80ETC concert'
+          if @sell_in < 11
+            if @quality < 50
+              @quality += 1
             end
           end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
+          if @sell_in < 6
+            if @quality < 50
+              @quality += 1
             end
           end
         end
       end
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
+    if @name != 'Sulfuras, Hand of Ragnaros'
+      @sell_in -= 1
     end
-    if item.sell_in < 0
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
+    if @sell_in < 0
+      if @name != 'Aged Brie'
+        if @name != 'Backstage passes to a TAFKAL80ETC concert'
+          if @quality > 0
+            if @name != 'Sulfuras, Hand of Ragnaros'
+              @quality -= 1
             end
           end
         else
-          item.quality = item.quality - item.quality
+          @quality = @quality - @quality
         end
       else
-        if item.quality < 50
-          item.quality += 1
+        if @quality < 50
+          @quality += 1
         end
       end
     end
