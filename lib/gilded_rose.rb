adebd71e673884dd55c4da893ff58dcd11d02bbe
Joel Weber <hobberwickey@gmail.com>
2015-10-13 12:32:15 -0400

    Adding in item.rb
    Creating more flexibility by adding a configurable item class.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,61 +1,72 @@
+require 'item'
+
 class GildedRose
   attr_reader :name, :days_remaining, :quality
 
-  RETAINED_QUALITY = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros']
-  GAIN_QUALITY = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert']
-  GAIN_QUALITY_AS_NEAR = ['Backstage passes to a TAFKAL80ETC concert']
-  BETTER_WITH_AGE = ['Aged Brie']
-  WORTHLESS_WITH_AGE = ['Backstage passes to a TAFKAL80ETC concert']
-  ETERNAL = ['Sulfuras, Hand of Ragnaros']
+  CONFIGS = {
+    'Aged Brie': {
+      retained_quality: true,
+      gains_quality: true,
+      continuous_quality: true
+    },
+    'Backstage passes to a TAFKAL80ETC concert': {
+      retained_quality: true,
+      gains_quality: true,
+      accelerating_quality: true,
+      hard_expiration: true
+    },
+    "Sulfuras, Hand of Ragnaros": {
+      retained_quality: true,
+      eternal: true
+    }
+  }
+
+  # RETAINED_QUALITY = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros']
+  # GAIN_QUALITY = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert']
+  # GAIN_QUALITY_AS_NEAR = ['Backstage passes to a TAFKAL80ETC concert']
+  # BETTER_WITH_AGE = ['Aged Brie']
+  # WORTHLESS_WITH_AGE = ['Backstage passes to a TAFKAL80ETC concert']
+  # ETERNAL = ['Sulfuras, Hand of Ragnaros']
 
   def initialize(name, days_remaining, quality)
     @name = name
     @days_remaining = days_remaining
     @quality = quality
+
+    @item = Item.new( CONFIGS[@name] )
   end
 
   def tick
     #if @name 
 
-    decrement_quality
+    decrement_quality 
 
-    increment_quality if GAIN_QUALITY.include? @name
-    increment_quality if @days_remaining < 11 and GAIN_QUALITY_AS_NEAR.include? @name
-    increment_quality if @days_remaining < 6 and GAIN_QUALITY_AS_NEAR.include? @name
+    increment_quality if @item.config[:gains_quality]
+    increment_quality if @days_remaining < 11 and @item.config[:accelerating_quality]
+    increment_quality if @days_remaining < 6 and @item.config[:accelerating_quality]
     
     decrement_remaining
 
     if @days_remaining < 0
-      increment_quality if BETTER_WITH_AGE.include? @name
-      @quality = 0 if WORTHLESS_WITH_AGE.include? @name
+      increment_quality if @item.config[:continuous_quality]
+      destroy_quality if @item.config[:hard_expiration]
       decrement_quality 
-      #if @name != 'Aged Brie'
-        # if @name != 'Backstage passes to a TAFKAL80ETC concert'
-          # if @quality > 0
-          #   if @name != 'Sulfuras, Hand of Ragnaros'
-          #     @quality -= 1
-          #   end
-          # end
-        # else
-          # @quality = @quality - @quality
-        # end
-      #else
-        # if @quality < 50
-        #   @quality += 1
-        # end
-      #end
     end
   end
 
+  def destroy_quality
+    @qualit = 0
+  end
+
   def decrement_quality
-    @quality -= 1 if @quality > 0 and not RETAINED_QUALITY.include? @name
+    @quality -= 1 if @quality > 0 and not @item.config[:retained_quality]
   end
 
   def increment_quality
-    @quality += 1 if quality < 50
+    @quality += 1 if quality < 50 
   end
 
   def decrement_remaining
-    @days_remaining -= 1 unless ETERNAL.include? @name
+    @days_remaining -= 1 unless @item.config[:eternal]
   end
 end
---
+++ lib/item.rb
@@ -0,0 +1,20 @@
+class Item
+  DEFAULT_CONFIG = {
+    retained_quality: false,
+    gains_quality: false,
+    accelerating_quality: false,
+    continuous_quality: false,
+    hard_expiration: false,
+    eternal: false
+  }
+
+  attr_accessor :config
+
+  def initialize(config={})
+    set_config(config)
+  end
+
+  def set_config(config)
+    @config = DEFAULT_CONFIG.merge(config)
+  end
+end
