f5ce4107376f45db3dae14c14162c6aa475b6663
Jordan Ryan Reuter <me@jreut.com>
2015-10-13 00:19:06 -0400

    Delete the scary if statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -43,48 +43,5 @@ class GildedRose
       end
       return
     end
-    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAFKAL80ETC concert'
-      if @quality > 0
-        if @name != 'Sulfuras, Hand of Ragnaros'
-          @quality -= 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality += 1
-        if @name == 'Backstage passes to a TAFKAL80ETC concert'
-          if @days_remaining < 11
-            if @quality < 50
-              @quality += 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality += 1
-            end
-          end
-        end
-      end
-    end
-    if @name != 'Sulfuras, Hand of Ragnaros'
-      @days_remaining -= 1
-    end
-    if @days_remaining < 0
-      if @name != 'Aged Brie'
-        if @name != 'Backstage passes to a TAFKAL80ETC concert'
-          if @quality > 0
-            if @name != 'Sulfuras, Hand of Ragnaros'
-              @quality -= 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality += 1
-        end
-      end
-    end
   end
 end
