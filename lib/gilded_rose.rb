8ad7ba7d3d8c369c97d6a97f0a76dd3c3d98c72a
Jordan Ryan Reuter <me@jreut.com>
2015-10-13 00:23:43 -0400

    refactor extracted methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,35 +10,24 @@ class GildedRose
   def normal_tick
     @days_remaining -= 1
     return if @quality <= 0
-    if @days_remaining >= 4
-      @quality -= 1
-    else
-      @quality -= 2
-    end
+    @quality -= 1
+    @quality -= 1 if @days_remaining < 4
   end
 
   def brie_tick
     @days_remaining -= 1
-    if @days_remaining < 0
-      @quality += 2
-    else
-      @quality += 1
-    end
-    return @quality = 50 if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining < 0
+    @quality = 50 if @quality >= 50
   end
 
   def backstage_tick
     @days_remaining -= 1
-    if @days_remaining >= 10
-      @quality += 1
-    elsif @days_remaining < 10 && @days_remaining >= 5
-      @quality += 2
-    elsif @days_remaining < 5 && @days_remaining >= 0
-      @quality += 3
-    else
-      @quality = 0
-    end
-    return @quality = 50 if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+    @quality = 0 if @days_remaining < 0
+    @quality = 50 if @quality >= 50
   end
 
   def tick
